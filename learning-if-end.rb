ebc9e123ee6774d91bee658d551822de9e46cb94
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-06 04:13:37 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,40 @@
+# run_code_inside = true
+# puts"code before if...end"
+# if run_code_inside
+#  puts"code inside"
+# end
+# puts "Code after.end"
+
+#chance_of_rain = 2.0
+#if chance_of_rain <= 0.25
+ # puts "Pack  a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+ # puts "Pack an umbrella!"
+#else
+ # puts "Stay home and read Hegel."
+#end
+
+def addition(num1 = 5 , num2 = 4)
+  num1 + num2 
+end
+
+def subtraction(num1c = 10 , num2 = 5)
+  num2 - num1
+end
+
+def division(num1 = 50 , num2 = 2)
+  num1 / num2
+end
+
+def multiplication(num1 = 4, num2 = 30)
+  num1 * num2
+
+end
+
+def modulo(num1 = 34, num2 = 5)
+  num1 % num2
+end
+
+def square_root(num = 81)
+  Math.sqrt = num
+end
